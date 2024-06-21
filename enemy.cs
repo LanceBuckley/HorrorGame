4f98d52115f47c96149331fa175576bbf952cfcb
Lance Buckley <erchancy@gmail.com>
2024-06-20 22:40:14 -0500

    The enemy, after extensive testing, can now avoid walls and find the player on either side of the map. He still struggles with individually placed walls that are not part of an office scene

--- enemy.cs
+++ enemy.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class enemy : CharacterBody3D
 {
@@ -7,11 +9,21 @@ public partial class enemy : CharacterBody3D
 	public const float Speed = 5.0f;
 	[Export] public player Player;
 	private Vector3 _directionToPlayer;
+	private enemy_area _enemyArea;
+	private Dictionary<ulong, Vector3> _colliders = new();
+	private bool _isCorner;
+	private float _greaterX;
+	private float _greaterZ;
+	private int _repeatedXCollision = 0;
+	private int _repeatedZCollision = 0;
 
 	public override void _Ready()
 	{
+		_enemyArea = GetNode<enemy_area>("EnemyArea");
 		_globals = GetNode<globals>("/root/Globals");
 		_globals.Connect(nameof(globals.PlayerAddedEventHandler), new Callable(this, nameof(FindPlayer)));
+		_enemyArea.Connect(nameof(enemy_area.CollisionEventHandler), new Callable(this, nameof(OnColliding)));
+		_enemyArea.Connect(nameof(enemy_area.LeavingEventHandler), new Callable(this, nameof(OnLeaving)));
 	}
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,22 +31,95 @@ public partial class enemy : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		Vector3 velocity = Velocity;
+		
 		// Calculate direction towards the player
 		_directionToPlayer = (Player.GlobalTransform.Origin - GlobalTransform.Origin).Normalized();
 		
-		//float XChange = Mathf.Lerp(Position.X, _playerPosition.X);
-		//float ZChange = Mathf.Lerp(Position.Z, _playerPosition.Z);
+		
 
-		Vector3 velocity = Velocity;
+		if (_colliders.Count == 0)
+		{
+			_isCorner = false;
+			velocity.X = _directionToPlayer.X * Speed;
+			velocity.Z = _directionToPlayer.Z * Speed;
+		}
+		
+		if (_colliders.Count == 1 && !_isCorner)
+		{
+			var collidersList = _colliders.ToList();
+			_greaterX = Mathf.Max(collidersList[0].Value.X, Mathf.Abs(_directionToPlayer.X));
+			_greaterZ = Mathf.Max(collidersList[0].Value.Z, Mathf.Abs(_directionToPlayer.Z));
+			if (_greaterX == Mathf.Abs(_directionToPlayer.X))
+			{
+				velocity.Z = -1 * Speed;
+			}
+			if (_greaterZ == Mathf.Abs(_directionToPlayer.Z))
+			{
+				velocity.X= -1 * Speed;
+			}
+		}
+		
+		if (_colliders.Count == 2)
+		{
+			_isCorner = true;
+			
+			var collidersList = _colliders.ToList();
+			_greaterX = Mathf.Max(Mathf.Abs(collidersList[0].Value.X), Mathf.Abs(collidersList[1].Value.X));
+			_greaterZ = Mathf.Max(Mathf.Abs(collidersList[0].Value.Z), Mathf.Abs(collidersList[1].Value.Z));
+			if (_repeatedXCollision > 20)
+			{
+				velocity.Z = -1 * Speed;
+			}
+			if (_repeatedZCollision > 20)
+			{
+				velocity.X = -1 * Speed;
+			}
+			else if (_greaterX > _greaterZ)
+			{
+				_repeatedXCollision += 1;
+				_repeatedZCollision = 0;
+				velocity.Z = 1 * Speed;
+			}
+			else if (_greaterZ > _greaterX)
+			{
+				_repeatedZCollision += 1;
+				_repeatedXCollision = 0;
+				velocity.X = 1 * Speed;
+			}
+		}
+
+
+		GD.Print("Player Direction" + _directionToPlayer);
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y -= Gravity * (float)delta;
 
-		velocity.X = _directionToPlayer.X * Speed;
-		velocity.Z = _directionToPlayer.Z * Speed;
+		//velocity.X = (_directionToPlayer.X * _collider.X) * Speed;
+		//velocity.Z = (_directionToPlayer.Z * _collider.Z) * Speed;
+		
+		//velocity.X = Mathf.MoveToward(_collider.X, _directionToPlayer.X, Speed);
+		//velocity.Z = Mathf.MoveToward(_collider.Z, _directionToPlayer.Z, Speed);
+		//velocity.X = -_collider.X * Speed;
+		//velocity.Z = -_collider.Z * Speed;
+			// velocity.X = Mathf.MoveToward(_collider.X, _directionToPlayer.X, Speed);
+			// velocity.Z = Mathf.MoveToward(_collider.Z, _directionToPlayer.Z, Speed);
+		/*else
+		{
+			velocity.X = _directionToPlayer.X + _collider.Z * Speed;
+			velocity.Z = _directionToPlayer.Z + _collider.X * Speed;
+		}*/
+		
+		
 
 		Velocity = velocity;
+		GD.Print("Velocity: " + Velocity);
+		GD.Print("Colliders: " + _colliders.Count);
+		foreach (var collider in _colliders)
+		{
+			GD.Print("Collider" + collider.Key);
+		}
 		MoveAndSlide();
 	}
 
@@ -46,5 +131,15 @@ public partial class enemy : CharacterBody3D
 	public player GetPlayer()
 	{
 		return Player;
+	}
+
+	private void OnColliding(ulong colliderId, Vector3 colliderPosition)
+	{
+		_colliders.Add(colliderId, colliderPosition);
+	}
+	
+	private void OnLeaving(ulong colliderId)
+	{
+		_colliders.Remove(colliderId);
 	}
 }
--- enemy_area.cs
+++ enemy_area.cs
@@ -6,12 +6,19 @@ public partial class enemy_area : Area3D
 {
 	private player _player;
 	private enemy _enemy;
+	private Vector3 _directionToCollider;
+	[Signal]
+	public delegate void CollisionEventHandler(ulong colliderId, Vector3 colliderDirection);
+	public delegate void LeavingEventHandler(ulong colliderId);
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		AddUserSignal(nameof(CollisionEventHandler));
+		AddUserSignal(nameof(LeavingEventHandler));
 		_enemy = GetParent<enemy>();
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+		Connect("body_exited", new Callable(this, nameof(OnBodyExited)));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,11 +33,19 @@ public partial class enemy_area : Area3D
 		_player ??= _enemy.GetPlayer();
 		if (body != _player)
 		{
-			GD.Print(body);
+			// We want to adjust the enemy velocity to avoid walls. If the area collides with a wall to its front/back(Z) then we move left or right(X) until we exit the wall and vice-versa
+			_directionToCollider = (body.GlobalTransform.Origin - _enemy.GlobalTransform.Origin).Normalized();
+			EmitSignal(nameof(CollisionEventHandler), body.GetInstanceId(), _directionToCollider);
 		}
 		else
 		{
 			GD.Print("Tag!");
 		}
+	}
+
+	private void OnBodyExited(Node3D body)
+	{
+		EmitSignal(nameof(LeavingEventHandler), body.GetInstanceId());
+		GD.Print("Exited" + body.Name);
 	}
 }
